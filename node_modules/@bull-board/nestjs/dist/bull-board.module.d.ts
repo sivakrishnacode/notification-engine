0cde1bbfbaf50a496ef2dbe06ca1f60d4250c0cf
sivakrishnacode <sivakrishnacoc@gmail.com>
2026-05-07 17:16:27 +0530

    remove node_modules from tracking

--- node_modules/@bull-board/nestjs/dist/bull-board.module.d.ts
+++
@@ -1,7 +0,0 @@
-import { DynamicModule } from "@nestjs/common";
-import { BullBoardModuleAsyncOptions, BullBoardModuleOptions, BullBoardQueueOptions } from "./bull-board.types";
-export declare class BullBoardModule {
-    static forFeature(...queues: BullBoardQueueOptions[]): DynamicModule;
-    static forRoot(options: BullBoardModuleOptions): DynamicModule;
-    static forRootAsync(options: BullBoardModuleAsyncOptions): DynamicModule;
-}
--- node_modules/@bull-board/nestjs/dist/bull-board.util.d.ts
+++
@@ -1,3 +0,0 @@
-import { BullBoardExpressAdapter, BullBoardFastifyAdapter, BullBoardServerAdapter } from "./bull-board.types";
-export declare const isFastifyAdapter: (adapter: BullBoardServerAdapter) => adapter is BullBoardFastifyAdapter;
-export declare const isExpressAdapter: (adapter: BullBoardServerAdapter) => adapter is BullBoardExpressAdapter;
